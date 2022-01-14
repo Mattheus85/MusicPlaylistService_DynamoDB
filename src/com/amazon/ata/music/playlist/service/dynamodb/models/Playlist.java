9ccfc45303aa60568573673b8d7a273cc52908bd
Matthew Mattheus <19mattheus85@gmail.com>
2022-01-13 20:36:04 -0800

    MT1 Tests 30/30 local

--- src/com/amazon/ata/music/playlist/service/dynamodb/models/Playlist.java
+++ src/com/amazon/ata/music/playlist/service/dynamodb/models/Playlist.java
@@ -8,6 +8,8 @@ import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBTable;
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBTypeConverted;
 
 import java.util.List;
+import java.util.Objects;
+import java.util.Set;
 
 /**
  * Represents a record in the playlists table.
@@ -15,7 +17,12 @@ import java.util.List;
 @DynamoDBTable(tableName = "playlists")
 public class Playlist {
     private String id;
+    private String name;
+    private String customerId;
+    private Integer songCount;
+    private Set<String> tags;
     private List<AlbumTrack> songList;
+
 
     @DynamoDBHashKey(attributeName = "id")
     public String getId() {
@@ -26,6 +33,43 @@ public class Playlist {
         this.id = id;
     }
 
+    @DynamoDBAttribute(attributeName = "name")
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @DynamoDBAttribute(attributeName = "customerId")
+    public String getCustomerId() {
+        return customerId;
+    }
+
+    public void setCustomerId(String customerId) {
+        this.customerId = customerId;
+    }
+
+    @DynamoDBAttribute(attributeName = "songCount")
+    public Integer getSongCount() {
+        return songCount;
+    }
+
+    public void setSongCount(Integer songCount) {
+        this.songCount = songCount;
+    }
+
+    @DynamoDBAttribute(attributeName = "tags")
+    public Set<String> getTags() {
+        return tags;
+    }
+
+    public void setTags(Set<String> tags) {
+        this.tags = tags;
+    }
+
+
     // PARTICIPANTS: You do not need to modify the songList getters/setters or annotations
     @DynamoDBTypeConverted(converter = AlbumTrackLinkedListConverter.class)
     @DynamoDBAttribute(attributeName = "songList")
@@ -35,5 +79,30 @@ public class Playlist {
 
     public void setSongList(List<AlbumTrack> songList) {
         this.songList = songList;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Playlist playlist = (Playlist) o;
+        return getId().equals(playlist.getId()) && getName().equals(playlist.getName()) && getCustomerId().equals(playlist.getCustomerId()) && getSongCount().equals(playlist.getSongCount()) && getTags().equals(playlist.getTags()) && getSongList().equals(playlist.getSongList());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId(), getName(), getCustomerId(), getSongCount(), getTags(), getSongList());
+    }
+
+    @Override
+    public String toString() {
+        return "Playlist{" +
+                "id='" + id + '\'' +
+                ", name='" + name + '\'' +
+                ", customerId='" + customerId + '\'' +
+                ", songCount=" + songCount +
+                ", tags=" + tags +
+                ", songList=" + songList +
+                '}';
     }
 }
